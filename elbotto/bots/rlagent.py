5cd9bf5c9caed31d1346b4477f815e0c1db81463
Gian-Marco Baschera <gmbaschera@gmail.com>
2020-09-12 19:51:10 +0200

    CNN2 with two convolutional layers

--- elbotto/bots/rlagent.py
+++ elbotto/bots/rlagent.py
@@ -82,6 +82,7 @@ class Bot(BaseBot):
                                           network=[
                                               [dict(type='retrieve', tensors=['cards']),
                                                dict(type='conv1d', size=256, window=9, stride=9, padding='valid'),
+                                               dict(type='conv1d', size=256, window=1, stride=1, padding='valid'),
                                                dict(type='flatten'),
                                                dict(type='register', tensor='cards-embedding')],
                                               [dict(type='retrieve', tensors=['features']),
